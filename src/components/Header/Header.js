ce67e07283502730a0560131b11294c140393ccd
Onuorah Ike <onuorahofficial@gmail.com>
2021-10-10 13:39:28 +0100

    updated responsiveness 1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div>
+    <div style={{ padding: '0 1rem' }}>
       <h1 style={{ color: '#2E2F31', marginTop: '1rem' }}>New Booking</h1>
       <p style={{ color: '#777', marginTop: '1rem' }}> fill in the information below to get a quote or create a new booking</p>
     </div>
